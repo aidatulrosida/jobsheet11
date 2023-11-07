0bf8ea1518678677e1fb9dae0997bbbda907b4dc
aidatulrosida <144088067+aidatulrosida@users.noreply.github.com>
2023-11-07 12:30:31 +0700

    Add files via upload

---
+++ Star02_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Star02_2 {
+    public static void main(String[] args) {
+        Scanner sc02 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = sc02.nextInt();
+
+        for(int i=1; i>N; i++){
+            System.out.print("*");
+        }
+    }
+}
